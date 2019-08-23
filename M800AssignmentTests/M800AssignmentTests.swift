66ee30f399a7848abf61eae9e01055cf4678ea5a
wuufone <wuufone@gmail.com>
2019-08-23 20:38:58 +0800

    重構。分析程式碼的變動頻率後，分離頻率不同的部分。 這裡變動可能性較大的，是： 1. 輸入的 term。 2. 取得搜尋結果後的處理。

--- M800AssignmentTests/M800AssignmentTests.swift
+++ M800AssignmentTests/M800AssignmentTests.swift
@@ -11,25 +11,29 @@ import XCTest
 
 class M800AssignmentTests: XCTestCase {
 
-    func testSearchAPI() {
+    let termString = "張雨生 我的未來不是夢"
+    
+    fileprivate func doTest(_ valueOfResultsItem: [[String : AnyObject]]) {
+        XCTAssertTrue(valueOfResultsItem.count == 1)
         
+        for aSong in valueOfResultsItem {
+            XCTAssertEqual(aSong["artistName"] as! String, "張雨生")
+            XCTAssertEqual(aSong["trackName"] as! String, "我的未來不是夢")
+        }
+    }
+    
+    func testSearchAPI() {
         do {
-            let responseData = try Data(contentsOf: ITunesSearchAPIHelper.url(queryItems:  ITunesSearchAPIHelper.queryItems(term: "張雨生 我的未來不是夢")))
+            let responseData = try Data(contentsOf: ITunesSearchAPIHelper.url(queryItems: ITunesSearchAPIHelper.queryItems(term: termString)))
             let responseJSONObject = try JSONSerialization.jsonObject(
                 with: responseData,
                 options: .allowFragments) as! Dictionary<String, AnyObject>
             for item in responseJSONObject {
                 if item.key == "results" {
                     let valueOfResultsItem = item.value as! Array<Dictionary<String, AnyObject>>
-                    XCTAssertTrue(valueOfResultsItem.count == 1)
-                    
-                    for aSong in valueOfResultsItem {
-                        XCTAssertEqual(aSong["artistName"] as! String, "張雨生")
-                        XCTAssertEqual(aSong["trackName"] as! String, "我的未來不是夢")
-                    }
+                    doTest(valueOfResultsItem)
                 }
             }
-            
         } catch {
             print(error.localizedDescription)
         }
