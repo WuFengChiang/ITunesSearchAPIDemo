2822fd9dcd0b58cae1e8107dd2ad318a3449b772
wuufone <wuufone@gmail.com>
2019-08-23 19:08:19 +0800

    Initial Commit

---
+++ M800Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M800Assignment
+//
+//  Created by wuufone on 2019/8/23.
+//  Copyright © 2019 江武峯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
